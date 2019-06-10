657a4304dc44c2d6fa42e324c0a4864db2e3d8b6
Masataro Asai <guicho2.71828@gmail.com>
2019-06-10 11:24:04 +0900

    idea

--- src/6linear-algebra.lisp
+++ src/6linear-algebra.lisp
@@ -42,64 +42,100 @@ NUMCL.  If not, see <http://www.gnu.org/licenses/>.
 (declaim (inline einsum))
 (defun einsum (subscripts &rest args)
   "Performs Einstein's summation.
-SUBSCRIPTS is a sequence of the form (<SPEC>+ [-> [<SPEC>*]]).
-The first set of specs before the `->` symbol specifies the input subscripts,
-and the second set of specs after `->` symbol specifies the output subscripts.
-Unlike Numpy, there can be multiple output subscripts
-and it performs two operations in the same loop, then return multiple values.
-The symbol `->` can be a string and can belong to any package
- because it is compared by STRING=.
+The SUBSCRIPT specification is significantly extended from that of Numpy
+and can be seens as a full-brown DSL for array operations.
+
+SUBSCRIPTS is a sequence of the form (<SPEC>+ [-> <TRANSFORM>*] [-> [<SPEC>*]).
+The remaining arguments ARGS contain the input arrays and optionally the output arrays.
+
+* SPEC
+
+The first set of SPECs specifies the input subscripts,
+and the second set of SPECs specifies the output subscripts.
+Unlike Numpy, there can be multiple output subscripts:
+It can performs multiple operations in the same loop, then return multiple values.
+The symbol `->` can be a string and can belong to any package because it is compared by STRING=.
 
 Each SPEC is an alphabetical string designator, such as a symbol IJK or a string \"IJK\",
- where each alphabet is considered as an index. It signals a type-error when it contains any
-non-alpha char. Alternatively, each SPEC can be a list that contains a list of symbols.
+where each alphabet is considered as an index. It signals a type-error when it contains any
+non-alpha char. 
 Note that a symbol NIL is interpreted as an empty list rather than N, I and L.
 
-The remaining arguments ARGS contains the input arrays and optionally the output arrays.
+Alternatively, each SPEC can be a list that contains a list of symbols.
+For example, '((i j) (j k) -> (i k)) and '(ij jk -> ik) are equivalent.
+
+When -> and the output SPECs are omitted, a single output is assumed and its spec is
+a union of the input specs.
+For example, '(ij jk) is equivalent to '(ij jk -> ijk).
+Note that '(ij jk) and '(ij jk ->) have the different meanings:
+The latter sums up all elements.
+
+* TRANSFORM
+
+TRANSFORM is a list of element-wise operations.
+The number of TRANSFORM should correspond to the number of outputs.
+In each TRANSFORM, the elements in the input arrays can be referenced by $N, where N is a 1-indexed number.
+Similarly the output array can be referred to by @N.
+
+For example, '(ij ik -> (+ @1 (* $1 $2)) -> ik) is equivalent to '(ij ik -> ik) (a GEMM).
+
+By default, TRANSFORM is (+ @1 (* $1 ... $N)) for N inputs, which is equivalent to Einstein's summation.
+
+* ARGS
+
 The shape of each input array should unify against the corresponding input spec. For example,
 with a spec IJI, the input array should be of rank 3 as well as
-the 1st and the 3rd dimensions of the input array should be the same.
+the 1st and the 3rd dimension of the input array should be the same.
 
 The shape of each output array is determined by the corresponding output spec.
 For example, if SUBSCRIPTS is '(ij jk -> ik), the output is an array of rank 2,
 and the output shape has the same dimension as the first input in the first axis,
 and the same dimension as the second input in the second axis.
 
-If provided, output array shapes and types are also checked against the
+If the output arrays are provided, their shapes and types are also checked against the
 corresponding output spec.  The types should match the result of the numerical
 operations on the elements of the input arrays.
 
-The output value is calculated in the following rule.
-+ First, the output array is initialized by 0.
+The outputs are calculated in the following rule.
+
++ The output array types are calculated based on the TRANSFORM, and 
+  the shapes are calcurated based on the SPEC and the input arrays.
++ The output arrays are allocated and initialized by zeros.
 + Einsum nests one loop for each index in the input specs.
   For example, '(ij jk -> ik) results in a triple loop.
-+ When an index used in the input spec is missing in the output spec,
-  the axis is aggregated over the iteration by summation.
++ In the innermost loop, each array element is bound to $1..$N / @1..@N.
++ For each @i, i-th TRANSFORM is evaluated and assigned to @i.
+
 + If the same index appears multiple times in a single spec,
   they share the same value in each iteration.
-  For example, '(ii -> i) returns the diagonal element of the matrix.
+  For example, '(ii -> i) returns the diagonal elements of the matrix.
+
+When TRANSFORMs are missing, it follows naturally from the default TRANSFORM values that
+
++ When an index used in the input spec is missing in the output spec,
+  the axis is aggregated over the iteration by summation.
 + If the same index appears across the different input specs,
   the element values from the multiple input arrays are aggregated by multiplication.
   For example, '(ij jk -> ik) will perform
   (setf (aref a2 i k) (* (aref a0 i j) (aref a1 j k)))
   when a0, a1 are the input arrays and a2 is the output array.
 
-When the separator -> is omitted, a single output is assumed and its spec is
-an alphabetically sorted indices of the union of the input specs.
-For example, '(ij jk) is equivalent to '(ij jk -> ijk), therefore
-(einsum '(ij jk) a b) is equivalent to :
+For example, (einsum '(ij jk) a b) is equivalent to:
 
- (dotimes (i <max> output)
+ (dotimes (i <max> <output>)
    (dotimes (j <max>)
      (dotimes (k <max>)
-       (setf (aref output i j k) (* (aref a i j) (aref b j k))))))
+       (setf (aref <output> i j k) (* (aref a i j) (aref b j k))))))
 
-Performace tips: If SUBSCRIPTS is a constant, the compiler macro
+* Performance
+
+If SUBSCRIPTS is a constant, the compiler macro
 builds an iterator function and make them inlined. Otherwise,
-a new function is made in each call to einsum, resulting in a huge bottleneck.
+a new function is made in each call to einsum, resulting in a large bottleneck.
+(It could be memoized in the future.)
 
-The nesting order of the loops depends on the spec.
-The nesting order affects the memory access pattern and therefore the performance due to
+The nesting order of the loops are automatically decided based on the specs.
+The order affects the memory access pattern and therefore the performance due to
 the access locality. For example, when writing a GEMM which accesses three matrices
 by (setf (aref output i j) (* (aref a i k) (aref b k j))),
 it is well known that ikj-loop is the fastest among other loops, e.g. ijk-loop.

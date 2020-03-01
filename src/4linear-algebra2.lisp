932b7653cfe8c07ca5fd7e27147b3cba3c09f25b
Masataro Asai <guicho2.71828@gmail.com>
2020-02-29 22:57:40 -0500

    fixup! matmul chain optimize

--- src/4linear-algebra2.lisp
+++ src/4linear-algebra2.lisp
@@ -158,12 +158,57 @@ NUMCL.  If not, see <http://www.gnu.org/licenses/>.
               (setf (aref a i j) (expt (aref v i) e)))))
       a)))
 
-
-;;; I jsut put all the necessary functions in here -- which is probably wrong
-;;; Maybe since they are all local I should have just made them local? That would make my function extremely
-;;; huge and unreadable though.
+;;;
 
 
+(defun matmul* (&rest args)
+  "Takes an arbitrary number of matrices and performs multiplication.
+It optimizes the multiplication order using the associativity."
+  (let* ((dimension-vector (map 'vector #'array-dimensions args))
+         (tree (car (%build-tree dimension-vector))))
+    (labels ((process-tree (tree)
+               (cond ((atom tree) (nth tree args))
+                     ((consp tree)
+                      (assert (= 2 (length tree)) nil "Tree must be binary")
+                      (list 'numcl:matmul
+                            (process-tree (first tree))
+                            (process-tree (second tree)))))))
+      (process-tree tree))))
+
+(define-compiler-macro matmul* (&whole form &rest args)
+  (if (= 2 (length args))
+      `(matmul ,@args)                   ;avoid runtime overhead
+      form))
+
+
+(defun %build-tree (dimensions)
+  "Builds an order tree of multiplications, dynamically solving it for all ranges."
+  (let* ((n (length dimensions))
+         (table (make-array (list n n))))
+    (loop :for i :from 0 :below n
+          :do (setf (aref table i i) (list i (aref dimensions i) 0)))
+    (loop :for i :from 0 :below (1- n)
+          :do (setf (aref table i (1+ i))
+                    (cons (list i (1+ i)) (%check-two-matrices (second (aref table i i))
+                                                               (second (aref table (1+ i) (1+ i)))))))
+    (loop :for len :from 2 :below n
+          :do (loop :for ind :from 0 :below (- n len)
+                    :do (setf (aref table ind (+ ind len))
+                              (loop :for step :from 1 :upto len
+                                    :with best := (cons (list (first (aref table ind ind))
+                                                              (first (aref table (1+ ind) (+ ind len))))
+                                                        (%add-two-cases
+                                                            (cdr (aref table ind ind))
+                                                            (cdr (aref table (1+ ind) (+ ind len)))))
+                                    :for next := (cons (list (first (aref table ind (+ ind (1- step))))
+                                                             (first (aref table (+ ind step) (+ ind len))))
+                                                       (%add-two-cases
+                                                        (cdr (aref table ind (+ ind (1- step))))
+                                                        (cdr (aref table (+ ind step) (+ ind len)))))
+                                    :when (< (third next) (third best))
+                                      :do (setf best next)
+                                    :finally (return best)))))
+    (aref table 0 (1- n))))
 
 (defun %check-two-matrices (first second)
   #.(format
@@ -199,60 +244,3 @@ NUMCL.  If not, see <http://www.gnu.org/licenses/>.
   (loop :for i :from 1 :below (length sizes)
         :with fh := (car (aref sizes 0))
         :sum (* fh (car (aref sizes i)) (cadr (aref sizes i)))))
-
-
-(defun %build-tree (dimensions)
-  "Builds an order tree of multiplications, dynamically solving it for all ranges."
-  (let* ((n (length dimensions))
-         (table (make-array (list n n))))
-    (loop :for i :from 0 :below n
-          :do (setf (aref table i i) (list i (aref dimensions i) 0)))
-    (loop :for i :from 0 :below (1- n)
-          :do (setf (aref table i (1+ i))
-                    (cons (list i (1+ i)) (%check-two-matrices (second (aref table i i))
-                                                               (second (aref table (1+ i) (1+ i)))))))
-    (loop :for len :from 2 :below n
-          :do (loop :for ind :from 0 :below (- n len)
-                    :do (setf (aref table ind (+ ind len))
-                              (loop :for step :from 1 :upto len
-                                    :with best := (cons (list (first (aref table ind ind))
-                                                              (first (aref table (1+ ind) (+ ind len))))
-                                                        (%add-two-cases
-                                                            (cdr (aref table ind ind))
-                                                            (cdr (aref table (1+ ind) (+ ind len)))))
-                                    :for next := (cons (list (first (aref table ind (+ ind (1- step))))
-                                                             (first (aref table (+ ind step) (+ ind len))))
-                                                       (%add-two-cases
-                                                        (cdr (aref table ind (+ ind (1- step))))
-                                                        (cdr (aref table (+ ind step) (+ ind len)))))
-                                    :when (< (third next) (third best))
-                                      :do (setf best next)
-                                    :finally (return best)))))
-    (aref table 0 (1- n))))
-
-(defmacro matmul-fast (&rest matrices)
-  (reduce (lambda (acc m)
-            (list 'numcl:matmul acc m))
-          (cdr matrices)
-          :initial-value (car matrices)))
-
-(define-compiler-macro matmul-fast (&whole form &rest arg-list)
-  (if (= 2 (length arg-list))
-      `(matmul-fast ,@arg-list)
-      (let* ((dimension-vector (map 'vector #'array-dimensions arg-list))
-             (tree (car (%build-tree dimension-vector))))
-        (loop :for pair-1 :across dimension-vector
-              :for pair-2 :across (subseq dimension-vector 1)
-              :do (assert (= (second pair-1)
-                             (first pair-2))
-                          nil
-                          "Illegal array dimensions for multiplication! Can't multiply ~A by ~A"
-                          pair-1 pair-2))
-        (labels ((process-tree (tree)
-                   (cond ((atom tree) (nth tree arg-list))
-                         ((consp tree)
-                          (assert (= 2 (length tree)) nil "Tree must be binary")
-                          (list 'numcl:matmul
-                                (process-tree (first tree))
-                                (process-tree (second tree)))))))
-          (process-tree tree)))))
